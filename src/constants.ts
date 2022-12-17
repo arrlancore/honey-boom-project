97a57eb46193c6ddf9d06372b260836fa2fab05f
arrlancore <arrlancore@gmail.com>
2022-12-17 15:22:48 +0700

    add constant

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = "http://18.142.84.66:3000";
+
+export const SIGNUP_URL = `${BASE_URL}/api/signup`;
+export const LOGIN_URL = `${BASE_URL}/api/login`;
+export const GPS_URL = `${BASE_URL}/api`;
+export const DEVICE_URL = `${BASE_URL}/api/{id}`;
